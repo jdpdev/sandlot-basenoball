df6f82f9919cdf43b0eb5a9cbccf6b5ad32b7aa7
Jason DuPertuis <gallahad@me.com>
2015-07-04 12:42:27 -0400

    Added handedness to players

--- js/playerInfo.js
+++ js/playerInfo.js
@@ -1,5 +1,11 @@
 function PlayerInfo(json) {
+	// The name of the player
 	this.name = json.name;
+
+	// True if right handed, false if not
+	this.handedness = json.hand;
+
+	// Stats
 	this.power = json.power;
 	this.batting = json.batting;
 	this.pitching = json.pitching;
